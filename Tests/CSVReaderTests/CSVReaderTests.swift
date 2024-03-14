9f8ed47ae2fa2f0b5e8220c9d8ce462d9d80aa76
Merch Visoiu <mvisoiu@wavelo.com>
2024-03-14 04:46:20 -0400

    Initial Commit

---
+++ Tests/CSVReaderTests/CSVReaderTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CSVReader
+
+final class CSVReaderTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CSVReader().text, "Hello, World!")
+    }
+}
