550cc3539ebb9acb1618d8f6314f4be4c9baf0f6
Merch Visoiu <mvisoiu@wavelo.com>
2024-03-15 09:46:08 -0400

    Test

--- Tests/CSVReaderTests/CSVReaderTests.swift
+++ Tests/CSVReaderTests/CSVReaderTests.swift
@@ -3,11 +3,11 @@ import XCTest
 
 class CSVReaderTests: XCTestCase {
     
-    func test_speed() throws { /// 1min 22s
+    func test_speed() throws {
         let reader = CSVReader()
-        let url = Bundle.module.url(forResource: "csv/stop_times.txt", withExtension: nil)!
-        let values = try reader.read(from: url, keys: ["trip_id", "stop_id"])
-        XCTAssert(values.count == 3742960)
+        let url = Bundle.module.url(forResource: "csv/stops.txt", withExtension: nil)!
+        let values = try reader.read(from: url, keys: ["stop_id", "stop_code", "stop_name", "stop_desc", "stop_lat", "stop_lon", "zone_id", "stop_url", "location_type", "parent_station", "platform_code"])
+        XCTAssert(values.count == 13038)
 
     }
 }
