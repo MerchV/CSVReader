fec6f259d1a8a1788eb800988438831f70387c64
Merch Visoiu <mvisoiu@wavelo.com>
2024-03-23 07:03:09 -0400

    Refactor to require the number of lines to read to avoid memory usage problem.

--- Sources/CSVReader/CSVReader.swift
+++ Sources/CSVReader/CSVReader.swift
@@ -14,9 +14,13 @@ public enum CSVReaderError: Error {
 }
 
 
-public struct CSVReader {
+public class CSVReader {
     
-    /// A range with quotes, or no comma, then no comma.
+    /// Represents a line in a CSV file, after the first header line.
+    /// Example 1: `a,b,c,d,e`. This will produce 5 matches, with `a`, `b`, `c`, and `d` in the second group, and `e` in the third group.
+    /// Example 2: `a,"b",c,d,e`. This will prouce 5 matches, with `a` in the second group, `b` in the first group without quotes, `c` and `d` in the second group, and `e` in the third group.
+    /// The `(?:)` is used to exclude the quotes in the first group, and the comma in the second group.
+    /// Explanation: Match a string with quotes separated by a comma but exclude the quotes and comma from the group, or match a string without a comma separated by a comma, or match a string without a comma until the end.
     private let PATTERN = "(?:\"(.*)\",)|(?:([^,]*),)|([^,]*)"
     
     /// The group with quotes.
@@ -28,53 +32,89 @@ public struct CSVReader {
     /// The trailing value after the final comma.
     private let THIRD_RANGE = 3
     
+    /// The keys in the header.
+    private let keys: [String]
     
-    public init() {}
+    /// Each line in the CSV after the header, and without any potentially empty lines, maybe the last line.
+    private var valueLines = [String]()
+    
+    /// The index into each line of the key.
+    private var keyToIndex = [String: Int]()
+    
+    /// Number of lines read into the CSV.
+    private var readLines = 0
     
     
-    /// read
-    /// - Parameter url: The local file location.
-    /// - Parameter keys: The header elements.
-    /// - Returns: An array of dictionaries representing each line in the CSV file.
-    public func read(from url: URL, keys: [String]) throws -> [[String: String]] {
-        var res = [[String: String]]()
+    /// Init.
+    /// - Parameters:
+    ///   - url: The local file.
+    ///   - keys: The headers.
+    ///   - separator: The whitespace characters at the end of each line, likely `\r` or `\n` or both.
+    public init(url: URL, keys: [String], separator: String? = "\r\n") throws {
+        self.keys = keys
         guard FileManager.default.fileExists(atPath: url.path) else { throw CSVReaderError.fileNotFound }
         
         let data = try Data(contentsOf: url)
         guard let string = String(data: data, encoding: .utf8) else { throw CSVReaderError.notAString }
-        var readFirstLine = false
-        var keyToIndex = [String: Int]()
-        let lines = string.components(separatedBy: "\r\n")
-        for line in lines {
-            
-            /// Skip empty lines.
-            guard line != "" else { continue }
-            
-            if readFirstLine == false {
-                readFirstLine = true
-                let lineComponents = line.components(separatedBy: ",")
-                let trimmedLineComponents = lineComponents.compactMap({ $0.trimmingCharacters(in: .whitespacesAndNewlines ) })
-                for keyString in keys {
-                    let index = trimmedLineComponents.firstIndex(of: keyString)
-                    keyToIndex[keyString] = index
-                }
-            } else {
-                var newMap = [String: String]()
-                let parsed = parse(line: line)
+        let lines = string.components(separatedBy: separator!)
+        guard let line = lines.first else { throw CSVReaderError.noFirstLine }
+        
+        /// Extract the keys from the header line.
+        self.keyToIndex = extractKeysFromHeader(line: line)
+        
+        /// Iterate through each line in the CSV file, after the header line, to check for empty lines.
+        let afterFirstLine = 1
+        for i in afterFirstLine..<lines.count {
                 
-                for keyString in keys {
-                    guard let index = keyToIndex[keyString] else { continue }
-                    guard index < parsed.count else { continue }
-                    let valueString = parsed[index]
-                    newMap[keyString] = valueString
-                }
-            
-                res.append(newMap)
+            /// Skip empty lines, probably just the last line.
+            if lines[i] != "" {
+                self.valueLines.append(lines[i])
             }
+        }
+    }
+    
+    /// Reading from the CSV is done incrementally since a very large CSV file can use a lot of memory. For example, a CSV file with 3,743,221 lines can use 23 GB of memory to convert into an array of dictionaries.
+    /// - Parameter lines: The number of lines to read.
+    /// - Returns: An array of dictionaries, each dictionary representing a line, with the key the text from the header.
+    public func next(lines: Int) throws -> [[String: String]] {
+        var res = [[String: String]]()
+        
+        /// Check that the requested number of lines does not exceed the number of lines in the CSV file.
+        let upto = readLines + lines > self.valueLines.count ? self.valueLines.count : readLines + lines
+        
+        for i in readLines..<upto {
+            let line = self.valueLines[i]
+            var newMap = [String: String]()
+            let parsed = parse(line: line)
+            
+            for keyString in keys {
+                guard let index = keyToIndex[keyString] else { continue }
+                guard index < parsed.count else { continue }
+                let valueString = parsed[index]
+                newMap[keyString] = valueString
+            }
+            
+            res.append(newMap)
+            
         }
         return res
     }
     
+    
+    /// Build a mapping of the keys from the header line to an index in order to find it on each line.
+    /// - Parameter line: The header line.
+    /// - Returns: A mapping for each header key to its position index.
+    private func extractKeysFromHeader(line: String) -> [String: Int] {
+        let lineComponents = line.components(separatedBy: ",")
+        let trimmedLineComponents = lineComponents.compactMap({ $0.trimmingCharacters(in: .whitespacesAndNewlines ) })
+        for keyString in keys {
+            let index = trimmedLineComponents.firstIndex(of: keyString)
+            keyToIndex[keyString] = index
+        }
+        return keyToIndex
+    }
+    
+    /// Match each line in the CSV file with the regular expression.
     private func parse(line: String) -> [String] {
         var res = [String]()
         let regex = try! NSRegularExpression(pattern: PATTERN)
--- Tests/CSVReaderTests/CSVReaderTests.swift
+++ Tests/CSVReaderTests/CSVReaderTests.swift
@@ -3,11 +3,21 @@ import XCTest
 
 class CSVReaderTests: XCTestCase {
     
+    /// `stops.txt` has 13038 lines, after the header and minus the empty last line.
     func test_speed() throws {
-        let reader = CSVReader()
         let url = Bundle.module.url(forResource: "csv/stops.txt", withExtension: nil)!
-        let values = try reader.read(from: url, keys: ["stop_id", "stop_code", "stop_name", "stop_desc", "stop_lat", "stop_lon", "zone_id", "stop_url", "location_type", "parent_station", "platform_code"])
+        let reader = try CSVReader(url: url, keys: ["stop_id", "stop_code", "stop_name", "stop_desc", "stop_lat", "stop_lon", "zone_id", "stop_url", "location_type", "parent_station", "platform_code"])
+        let values = try reader.next(lines: 13039) /// Ask for too many lines, but that shouldn't cause an index-out-of-bounds exception.
         XCTAssert(values.count == 13038)
+
+    }
+    
+    /// Consumes 23 GB of memory. The file `stop_times.txt` is git ignored since it is 210 MB.
+    func test_memory() throws {
+        let url = Bundle.module.url(forResource: "csv/stop_times.txt", withExtension: nil)!
+        let reader = try CSVReader(url: url, keys: ["trip_id", "stop_id"])
+        let values = try reader.next(lines: 1624557)
+        XCTAssert(values.count == 1624557)
 
     }
 }
